8e8e125b55578c76dee74bf7270324844f323fe5
ALunGame <925773930@qq.com>
2023-12-24 17:18:01 +0800

    地图

--- MineGame/Plugins/IAFramework/Intermediate/Build/Win64/UnrealEditor/Inc/IAFramework/UHT/IAFramework.init.gen.cpp
+++ MineGame/Plugins/IAFramework/Intermediate/Build/Win64/UnrealEditor/Inc/IAFramework/UHT/IAFramework.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeIAFramework_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x9CCB2F5E,
+				0xFD5EA901,
 				0xA5446086,
 				METADATA_PARAMS(nullptr, 0)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeIAFramework_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_IAFramework.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_IAFramework(Z_Construct_UPackage__Script_IAFramework, TEXT("/Script/IAFramework"), Z_Registration_Info_UPackage__Script_IAFramework, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x9CCB2F5E, 0xA5446086));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_IAFramework(Z_Construct_UPackage__Script_IAFramework, TEXT("/Script/IAFramework"), Z_Registration_Info_UPackage__Script_IAFramework, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xFD5EA901, 0xA5446086));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
